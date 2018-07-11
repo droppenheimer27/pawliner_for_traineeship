5aa6a76a147174becb4b8368fb2f6e1088e16931
Vladislav Pavlowski <droppenheimer27@gmail.com>
2018-07-12 02:28:19 +0300

    done with sample home page

---
+++ Pawliner/Areas/HelpPage/ModelDescriptions/SimpleTypeModelDescription.cs
@@ -0,0 +1,6 @@
+namespace Pawliner.Areas.HelpPage.ModelDescriptions
+{
+    public class SimpleTypeModelDescription : ModelDescription
+    {
+    }
+}
---
+++ Pawliner/Scripts/pawliner.js
@@ -0,0 +1,30 @@
+﻿
+$("#registerButton").click(function () {
+    $("#loginBlock").hide();
+    $("#registerBlock").show();
+});
+
+$("#loginButton").click(function () {
+    $("#registerBlock").hide();
+    $("#loginBlock").show();
+});
+
+$('.dropdown-menu').on('click', function (event) {
+    var events = $._data(document, 'events') || {};
+    events = events.click || [];
+
+    for (var i = 0; i < events.length; i++) {
+        if (events[i].selector) {
+
+            
+            if ($(event.target).is(events[i].selector)) {
+                events[i].handler.call(event.target, event);
+            }
+
+            $(event.target).parents(events[i].selector).each(function () {
+                events[i].handler.call(this, event);
+            });
+        }
+    }
+    event.stopPropagation();
+});
